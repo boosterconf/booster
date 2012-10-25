97b52af52e416cfcf8c75a53f86c1e21a60d3df9
kjerstibb <rootsconf@gmail.com>
2012-10-25 20:27:48 +0200

    Something

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
                   :current_login_at, :current_login_ip, :description, :dietary_requirements, :email, :password, :password_confirmation,
                   :failed_login_count, :feature_as_organizer, :featured_speaker, :female, :hometown,
                   :invited, :is_admin, :last_login_at, :last_request_at, :login_count, :member_dnd, :name,
-                  :password_salt, :perishable_token, :persistence_token, :phone_number, :registration_ip, :role,
+                  :password_salt, :perishable_token, :persistence_token, :phone_number, :registration_ip, :role, :roles,
                   :registration_attributes, :bio_attributes
 
   has_one :registration
@@ -52,6 +52,10 @@ class User < ActiveRecord::Base
 
   def user_status
     registration ? registration.description : "Ukjent"
+  end
+
+  def roles_description
+    roles.split(",").map {|r| Roles.label[r] }
   end
 
   def deliver_password_reset_instructions!
