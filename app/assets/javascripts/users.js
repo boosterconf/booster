9411ea6a039708d81c029ff02ec8ed389481ef39
kjerstibb <rootsconf@gmail.com>
2012-11-04 13:50:47 +0100

    Working on the layout

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,3 @@
 $(document).ready(function () {
-        $('#manual_payment input').on('click', function (event) {
-            $('#invoice_fields').fadeIn();
-            $('#register_submit input').val('Register');
-        });
-
-        $('#paypal input').on('click', function (event) {
-            $('#invoice_fields').fadeOut();
-            $('#register_submit input').val('Register and proceed to payment');
-        });
-
-    }
-);
+    $('.error input:first').focus();
+});
--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,24 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Booster2013::Application.initialize!
 
-ActionView::Base.field_error_proc = proc {|html, instance| html }
+## We do not want to leak user names, so for login
+## do not mark the fields with errors, just display
+## a generic message instead.
+ActionView::Base.field_error_proc = proc do |html, instance|
+  if instance.object_name == 'user_session'
+    html
+  else
+    if html =~ /^<label/
+      html = %(<div class="clearfix error">#{html}</div>).html_safe
+    elsif html =~ /<input/
+      if instance.error_message.kind_of?(Array)
+        html = %(<div class="clearfix error">#{html}<span class="help-inline">&nbsp;#{instance.error_message.join(',')}</span></div>).html_safe
+      else
+        html = %(<div class="clearfix error">#{html}<span class="help-inline">&nbsp;#{instance.error_message}</span></div>).html_safe
+      end
+    end
+    html
+  end
+end
 
 ActionMailer::Base.delivery_method = :smtp
