ae1fe62ee382143c3400bd5c9f12c26d9a90ba83
kjerstibb <rootsconf@gmail.com>
2013-01-15 21:23:24 +0100

    something

--- app/controllers/send_regning.rb
+++ app/controllers/send_regning.rb
@@ -2,16 +2,24 @@ require 'securerandom'
 
 class SendRegning
 
-  def self.invoice_person(client_id, recipient_name, email, zipcode, city, ticket_type, ticket_price)
-    batch_id = SecureRandom.uuid
-    client = Sendregning::Client.new('kontakt@boosterconf.no', 'jySGOHz7', :test => true)
+  #Needs to support both email and paper invoice
+  #Needs to support user genereated reference
+  #Needs to support multiple order lines
+  #Participant name must be part of order line description.
 
-    invoice = client.new_invoice(
-        :name => recipient_name,
-        :zip => zipcode,
-        :city => city,
+  def initialize(test)
+    @client = Sendregning::Client.new('kontakt@boosterconf.no', 'jySGOHz7', :test => test)
+  end
+
+  def self.invoice_person(invoice)
+    batch_id = SecureRandom.uuid
+
+    invoice = @client.new_invoice(
+        :name => invoice.recipient_name,
+        :zip => invoice.zip,
+        :city => invoice.city,
         :shipment => :email,
-        :emailaddresses => email
+        :emailaddresses => invoice.email
     )
 
     invoice.add_line :qty => 1, :desc => 'Pony', :unitPrice => '1.00'
