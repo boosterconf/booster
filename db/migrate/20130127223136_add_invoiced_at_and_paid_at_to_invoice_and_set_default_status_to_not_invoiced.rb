d0502bc21a758486e2c82fffb754718496efc81f
Karianne Berg <karianne.berg@gmail.com>
2013-01-28 01:25:29 +0100

    typo

--- db/migrate/20130127223136_add_invoiced_at_and_paid_at_to_invoice_and_set_default_status_to_not_invoiced.rb
+++ db/migrate/20130127223136_add_invoiced_at_and_paid_at_to_invoice_and_set_default_status_to_not_invoiced.rb
@@ -1,6 +1,6 @@
 class AddInvoicedAtAndPaidAtToInvoiceAndSetDefaultStatusToNotInvoiced < ActiveRecord::Migration
   def change
-    add_column :invoices, :invoiced_at, :timestmp
+    add_column :invoices, :invoiced_at, :timestamp
     add_column :invoices, :paid_at, :timestamp
 
     change_column :invoices, :status, :string, :default => "not_invoiced"
