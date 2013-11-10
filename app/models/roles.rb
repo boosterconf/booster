f0091cca439787500f7e2601816a24d01082c215
Karianne Berg <karianne.berg@gmail.com>
2013-11-10 19:55:24 +0100

    added product developer to roles

--- app/models/roles.rb
+++ app/models/roles.rb
@@ -1,14 +1,17 @@
 module Roles
   def self.all
-    [:developer, :tester, :project_manager, :architect, :ux_specialist, :security_professional]
+    [:developer, :tester, :project_manager, :architect, :ux_specialist, :security_professional, :product]
   end
 
   def self.label
-    {:developer => "Developer",
-     :tester => "Tester/test lead",
-     :project_manager => "Project manager",
-     :architect => "Architect",
-     :ux_specialist => "UX specialist",
-     :security_professional => "Security professional"}
+    {
+        developer: 'Developer',
+        tester: 'Tester/test lead',
+        project_manager: 'Project manager',
+        architect: 'Architect',
+        ux_specialist: 'UX specialist',
+        security_professional: 'Security professional',
+        product: 'Product developer'
+    }
   end
 end
