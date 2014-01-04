208acb5a3e833aff41860df1241db82c75d079ba
Karianne Berg <karianne.berg@gmail.com>
2014-01-04 01:02:46 +0100

    Removed limit on review length

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131211092359) do
+ActiveRecord::Schema.define(:version => 20140103235513) do
 
   create_table "bios", :force => true do |t|
     t.integer   "user_id"
@@ -153,7 +153,7 @@ ActiveRecord::Schema.define(:version => 20131211092359) do
     t.integer  "talk_id"
     t.integer  "reviewer_id"
     t.string   "subject"
-    t.string   "text"
+    t.text     "text"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
