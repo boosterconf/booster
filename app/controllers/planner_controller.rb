e1c7d2f84de6fdbc32842455d54fdc9867ecded5
Nina Andersen <nina@vimond.com>
2014-01-10 18:39:23 +0100

    Added time table

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -3,7 +3,8 @@ class PlannerController < ApplicationController
   before_filter :require_admin
 
   def index
-
+    @days = ["Wednesday 12th", "Thursday 13th",  "Friday 14th"]
+    @num_tracks = 7
   end
 
 end
