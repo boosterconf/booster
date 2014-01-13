244cb60838a4d8a7703ca1cbdf342559707b485c
Karianne Berg <karianne.berg@gmail.com>
2014-01-13 15:58:14 +0100

    Fixes the listing of actual lightning talks. Breaks the dragging and dropping. :/

--- app/assets/javascripts/backbone/views/speak_item_list_view.js
+++ app/assets/javascripts/backbone/views/speak_item_list_view.js
@@ -1,30 +1,22 @@
 var SpeakItemListView = Backbone.View.extend({
 
-    // HTML element name, where to render a view.
-    el: '#speaks_listing',
-
-    initialize: function() {
-        this.collection = new SpeakCollection;
-        this.collection.fetch({reset: true}); // NEW
-        this.render();
-    },
-
-    // Render view.
+    // Renders view
     render: function() {
         var html = '';
-        console.log(this.collection.models);
         _.each(this.collection.models,function(model,index,list) {
+
+            // TODO: Create model view
             var item_html =
                 '<div id="speak_box" draggable="true" class="draggable">' +
                 '<h4>' + model.get('title') + '</h4>' +
                     '<h6>' + "----" + '</h6>' +
-                    '</div>'
+                    '</div>';
             html = html + '<li>' + item_html + '</li>';
         });
 
         html = '<ul>' + html + '</ul>';
 
-        // Set html for the view element using jQuery.
-        $(this.el).html(html);
+        this.$el.html(html);
+        return this;
     }
 });
