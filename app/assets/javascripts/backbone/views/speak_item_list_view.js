4ee2f06b3a98d9cf3d50f24790d9c8b7a0b39da6
Karianne Berg <karianne.berg@gmail.com>
2014-01-20 22:08:57 +0100

    add company of speaker

--- app/assets/javascripts/backbone/views/speak_item_list_view.js
+++ app/assets/javascripts/backbone/views/speak_item_list_view.js
@@ -5,11 +5,12 @@ var SpeakItemListView = Backbone.View.extend({
         var html = '';
         _.each(this.collection.models, function(model, index, list) {
 
+            var speaker = model.get('users').first()
             // TODO: Create model view
             var item_html =
                 '<div id="speak_box" draggable="true" class="draggable">' +
                 '<h5>' + model.get('title') + '</h5>' +
-                    '<h6>' + model.get('users').first().get('name') + '</h6>' +
+                    '<h6>' + speaker.get('name') + ' (' + speaker.get('company') + ')' + '</h6>' +
                     '</div>';
             html = html + '<li>' + item_html + '</li>';
         });
