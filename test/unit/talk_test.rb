78985db99d8ac780603a2c615ec64d74e625c8b3
Karianne Berg <karianne.berg@gmail.com>
2014-01-24 23:02:11 +0100

    better naming in review form if you see own presentation

--- test/unit/talk_test.rb
+++ test/unit/talk_test.rb
@@ -1,50 +1,71 @@
 require 'test_helper'
 
 class TalkTest < ActiveSupport::TestCase
-  context "talk" do
-    should "increment comments count when adding a comment" do
+  context 'talk' do
+    should 'increment comments count when adding a comment' do
       talk = talks(:one)
       comments_count = talk.comments_count
-      talk.comments << Comment.new(:title => "en tittel", :description => "en beskrivelse", :user => User.last)
+      talk.comments << Comment.new(:title => 'en tittel', :description => 'en beskrivelse', :user => User.last)
       talk = Talk.find(talk.id)
       assert_equal comments_count + 1, talk.comments_count
     end
 
-    should "be pending by default" do
+    should 'be pending by default' do
       talk = Talk.new
       assert talk.pending?
     end
 
-    should "be accepted" do
+    should 'be accepted' do
       talk = Talk.new
       talk.accept!
+
       assert talk.accepted?
-      assert !talk.refused?
-      assert !talk.pending?
+      refute talk.refused?
+      refute talk.pending?
     end
 
-    should "be refused" do
+    should 'be refused' do
       talk = Talk.new
       talk.refuse!
+
       assert talk.refused?
-      assert !talk.accepted?
-      assert !talk.pending?
+      refute talk.accepted?
+      refute talk.pending?
     end
 
-    should "be able to regret" do
+    should 'be able to regret' do
       talk = Talk.new
       talk.regret!
       assert talk.pending?
-      assert !talk.refused?
-      assert !talk.accepted?
+      refute talk.refused?
+      refute talk.accepted?
     end
 
-    should "not list refused talks" do
+    should 'not list refused talks' do
       refused = talks(:one).refuse!
       talks(:two).accept!
       not_refused = Talk.all_pending_and_approved
-      assert !not_refused.include?(refused)
+
+      refute not_refused.include?(refused)
       assert not_refused.size > 0
+    end
+
+    context '#is_presented_by' do
+
+      should 'be true when user is amongst talk\'s speakers ' do
+        talk = talks(:three)
+
+        assert talk.is_presented_by?(talk.users.first)
+
+      end
+
+      should 'be false when user is not amongst talk\'s speakers ' do
+        talk = talks(:three)
+
+        refute talk.is_presented_by?(users(:god))
+
+      end
+
     end
 
 =begin
