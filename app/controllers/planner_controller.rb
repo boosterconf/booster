45e6555bdf658365b7d826ae12cd5a2d6d96be75
Karianne Berg <karianne.berg@gmail.com>
2014-01-26 12:25:27 +0100

    added two lightning talk slots instead of one. added border to table.

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -4,8 +4,13 @@ class PlannerController < ApplicationController
 
   def index
     @num_tracks = 3
-    @talks_per_track = 4
+    @talks_per_slot = 4
+    @num_slots = 2
     @talks = LightningTalk.all_with_speaker
+  end
+
+  def update
+
   end
 
 end
