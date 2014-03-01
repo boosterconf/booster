c7ed0b6238513899eb8ed087d54f6c0268ac73cb
André Heie Vik <andrev@fidrildi.com>
2014-03-01 08:09:02 +0100

    Added script for pulling database to postgres

---
+++ dbpull.sh
@@ -0,0 +1,24 @@
+# Replacement for heroku db:pull using pbbackups
+# 
+# Prerequisites:
+#
+# - Postgres installed and in path (OS X: use Postgres.app, add /Applications/Postgres.app/Contents/Versions/9.3/bin/ to path)
+# - Database created (initdb dbname)
+#
+# Usage:
+# dbpull.sh appname [dbname]
+#
+# If no dbname specified, appname is used as dbname
+# 
+set -e
+
+if [ -z "$1" ]; then 
+	echo "No app specified"
+	exit;
+fi
+
+db=${2-$1}
+
+heroku pgbackups:capture -a $1
+curl -o latest.dump `heroku pgbackups:url -a $1`
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d $db latest.dump
