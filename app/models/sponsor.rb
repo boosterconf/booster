b20dba9a89dcab8a7ab132cbfa0a29127df66a02
Erlend Birkenes <eb@dataloy.com>
2014-10-02 21:38:09 +0200

    Slett tilhørende events når sponsor blir slettet

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,7 +1,7 @@
 class Sponsor < ActiveRecord::Base
 
   belongs_to :user
-  has_many :events
+  has_many :events, :dependent => :destroy
 
   attr_accessible :comment, :contact_person_first_name, :contact_person_last_name, :contact_person_phone_number,
                   :email, :invoiced, :last_contacted_at, :location, :name, :paid, :status, :user_id,
