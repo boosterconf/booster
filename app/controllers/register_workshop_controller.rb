be5670190a9f157c76c29d97dee1b81b5e88f632
Erlend Birkenes <eb@dataloy.com>
2014-10-09 18:40:43 +0200

    Boosterbot har gode nyheter

--- app/controllers/register_workshop_controller.rb
+++ app/controllers/register_workshop_controller.rb
@@ -47,6 +47,7 @@ class RegisterWorkshopController < ApplicationController
       end
 
       BoosterMailer.talk_confirmation(current_user, @workshop, talk_url(@workshop)).deliver
+      SlackNotifier.notifyTalk(@workshop)
 
       if current_user.has_all_statistics
         redirect_to register_workshop_finish_url
