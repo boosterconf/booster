c3b7e11cd2bc6685b76fec641feafa3a8b2eeda4
Erlend Birkenes <erlend@birkenes.net>
2015-01-07 19:32:48 +0100

    Oppdatert forside med billettsalg

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Date::DATE_FORMATS[:short_ordinal] = lambda { |date| date.strftime("%B #{date.day.ordinalize}") }
+Time::DATE_FORMATS[:short_ordinal] = lambda { |date| date.strftime("%B #{date.day.ordinalize}") }
