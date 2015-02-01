e250a45cc443c32811120c531b6e5a1e95ce8fe8
Karianne Berg <karianne.berg@gmail.com>
2015-02-01 21:04:03 +0100

    Also add slot_in method.

--- app/models/period.rb
+++ app/models/period.rb
@@ -2,12 +2,18 @@ class Period < ActiveRecord::Base
 
   attr_accessible :day, :start_time, :end_time
 
-  has_many :slots
+  has_many :slots, order: 'room_id'
 
   validates_uniqueness_of :start_time, :scope => [:end_time, :day]
 
+  TYPES = %w(workshop lightning)
+
   def to_s
-    day.strftime("%a") + " " + start_time.strftime("%H:%M") + "-" + end_time.strftime("%H:%M")
+    day.strftime('%a') + ' ' + start_and_end_time
+  end
+
+  def start_and_end_time
+    start_time.strftime('%H:%M') + '-' + end_time.strftime('%H:%M')
   end
 
   def day_of_the_week
@@ -15,9 +21,13 @@ class Period < ActiveRecord::Base
   end
 
   def talk_in(room)
-    slot = slots.find { |slot| slot.room == room }
+    slot = slot_in(room)
     return nil unless slot
     slot.talk
+  end
+
+  def slot_in(room)
+    slots.find { |slot| slot.room == room }
   end
 
 end
