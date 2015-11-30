0a27c345d42ce46a7a2a43a00e6d995b0b5614ba
Karianne Berg <karianne.berg@gmail.com>
2015-11-30 22:53:37 +0100

    maybe fixed it?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150201161258) do
+ActiveRecord::Schema.define(:version => 20150209233348) do
 
   create_table "bios", :force => true do |t|
     t.integer  "user_id"
