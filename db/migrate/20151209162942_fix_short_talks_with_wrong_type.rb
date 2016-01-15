19d2ff1f84a65b044cc97072b39104f59b760fd9
Karianne Berg <karianne.berg@gmail.com>
2016-01-16 00:40:02 +0100

    Fix moronic migration that I made

--- db/migrate/20151209162942_fix_short_talks_with_wrong_type.rb
+++ db/migrate/20151209162942_fix_short_talks_with_wrong_type.rb
@@ -1,10 +1,8 @@
 class FixShortTalksWithWrongType < ActiveRecord::Migration
   def up
 
-    short_talk_type = TalkType.where(name: "Short talk").first
-
     Talk.includes(:talk_type).where(talk_types: { name: "Short talk" }).each do |talk|
-      talk.talk_type = short_talk_type
+      talk.type = "ShortTalk"
       talk.save!
     end
   end
