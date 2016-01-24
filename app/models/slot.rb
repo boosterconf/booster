7b25bc487d09cd3e23086859b62237f77af21f8c
Karianne Berg <karianne.berg@gmail.com>
2016-01-24 18:01:15 +0100

    Migrating from one to many talks in a slot: Step 2

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,16 +1,21 @@
 class Slot < ActiveRecord::Base
 
-  attr_accessible :period_id, :talk_id, :room_id
+  attr_accessible :period_id, :room_id
 
-  belongs_to :talk
+  has_many :talk_positions
+  has_many :talks, through: :talk_positions
   belongs_to :period
   belongs_to :room
 
   validates :period, presence: true
   validates :room, presence: true
-  validates :talk, presence: true
-  validates_uniqueness_of :room_id, scope: :period_id
   validate :talk_and_period_has_same_type
+
+
+  def talk
+    raise "Can't get talk on multi-talk slots" if talks.count > 1
+    talks.first
+  end
 
   def to_s
     "\"#{talk.title}\" at #{period}"
@@ -18,6 +23,7 @@ class Slot < ActiveRecord::Base
 
   private
   def talk_and_period_has_same_type
+    talk = Talk.find(talk_positions.first.talk_id)
     case period.period_type
     when "workshop"
       errors.add(:talk, "You tried to add a #{talk.talk_type.name.downcase} to a #{period.period_type.humanize} period") unless talk.is_workshop?
--- app/models/talk_position.rb
+++ app/models/talk_position.rb
@@ -2,5 +2,5 @@ class TalkPosition < ActiveRecord::Base
   belongs_to :talk
   belongs_to :slot
 
-  attr_accessible :talk, :slot, :talk_id, :slot_id
+  attr_accessible :talk, :slot, :talk_id, :slot_id, :position
 end
