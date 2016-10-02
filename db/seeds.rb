ac4c6b19191a7f318858f0b10e1675082a1827e6
kjersti <kjersti.berg@gmail.com>
2016-10-02 16:37:05 +0200

    No seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 require 'active_record/fixtures'
-ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/fixtures", %w(talk_types ticket_types))
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/fixtures", %w(talk_types))
