b69e70fe2912215e367e6cfcd5afa2331c628d3a
Anders Rørvik <anders.rorvik@gmail.com>
2016-10-07 15:25:05 +0200

    fixed syntax error and creation of file if it does not exist

--- provision/install-postgres.sh
+++ provision/install-postgres.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 postgresAptEntry="deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"
-sudo grep -q -F $postgresAptEntry /etc/apt/sources.list.d/pgdg.list || echo $postgresAptEntry | sudo tee -a >> /etc/apt/sources.list.d/pgdg.list
+aptSource="/etc/apt/sources.list.d/pgdg.list"
+
+if [ ! -f /etc/apt/sources.list.d/pgdg.list ]; then
+    touch /etc/apt/sources.list.d/pgdg.list
+fi
+
+sudo grep -q -F "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" /etc/apt/sources.list.d/pgdg.list || echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" | sudo tee -a >> $aptSource
 sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y postgresql-9.4 libpq-dev
@@ -12,4 +18,4 @@ host    all             all             0.0.0.0/0            trust
 host    all             all             ::/0                 trust' | sudo tee /etc/postgresql/9.4/main/pg_hba.conf
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.4/main/postgresql.conf
 sudo /etc/init.d/postgresql restart
-sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='vagrant'"  | grep -q 1 || sudo -u postgres -c 'createuser -s vagrant'
+sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='vagrant'"  | grep -q 1 || sudo su postgres -c 'createuser -s vagrant'
