8813cc7d7bb87d1524371d912cbec6a5411cccae
Anders Rørvik <anders.rorvik@gmail.com>
2017-02-19 22:16:32 +0100

    added full_name property to volunteer model.

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,9 @@
 class Volunteer < ActiveRecord::Base
   attr_accessible :first_name,:last_name, :email, :phone_number
   has_and_belongs_to_many :volunteers, join_table: 'talks_volunteers'
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
+
 end
