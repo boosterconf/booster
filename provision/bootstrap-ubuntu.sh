a203d3656c64f24db6576736f751578d5a86fec7
Karianne Berg <karianne.berg@gmail.com>
2017-05-20 14:30:10 +0200

    Add imagemagick install to Vagrant scripts

--- provision/bootstrap-ubuntu.sh
+++ provision/bootstrap-ubuntu.sh
@@ -5,4 +5,4 @@ sudo update-locale LC_ALL=en_US.UTF-8
 
 sudo apt-get update
 sudo apt-get install -y build-essential git curl libxslt1-dev libxml2-dev libssl-dev git
-
+sudo apt-get install imagemagick
