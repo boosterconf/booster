235dd983f43a8453abb16fb96cf504ee5e3302ef
anettebgo <anette.bergo@gmail.com>
2017-06-01 22:07:58 +0200

    cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-
-  #def test_user_with_one_pending_talk_does_not_have_all_talks_refused
-  #  god = users(:god)
-  #  assert !god.has_all_talks_refused?
-  #end
-
-  it "does not have all talks refused if one talk is pending" do
+describe User, type: :model do
+  it "does not say all talks refused if one talk is pending" do
     pending_talks_user = User.new
-    talk = Talk.new
-    talk.acceptance_status = 'pending'
-    pending_talks_user.talks << talk
+    pending_talks_user.talks << Talk.new({:acceptance_status => "pending"})
     expect(pending_talks_user.has_all_talks_refused?).to be_falsey
   end
 end
