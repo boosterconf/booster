44769dcb1e0c0a330bf5459a6cdde918bf5a5c83
anettebgo <anette.bergo@gmail.com>
2017-06-01 22:09:29 +0200

    i like this name better

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe User, type: :model do
   it "does not say all talks refused if one talk is pending" do
-    pending_talks_user = User.new
-    pending_talks_user.talks << Talk.new({:acceptance_status => "pending"})
-    expect(pending_talks_user.has_all_talks_refused?).to be_falsey
+    user_with_pending_talk = User.new
+    user_with_pending_talk.talks << Talk.new({:acceptance_status => "pending"})
+    expect(user_with_pending_talk.has_all_talks_refused?).to be_falsey
   end
 end
