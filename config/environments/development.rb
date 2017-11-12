95cffab97ebb8372f0ca7172f65482c298382e9f
Anders Rørvik <anders.rorvik@gmail.com>
2017-11-12 18:15:56 +0100

    show email of co-speaker if cospeaker is not fully registered

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Booster2013::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
   
   config.action_mailer.default_url_options = {
       :host => '127.0.0.1',
