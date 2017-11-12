8c53007e20e761e04d5bb27d220f70657ecc709a
Anders Rørvik <anders.rorvik@gmail.com>
2017-11-12 21:01:09 +0100

    do not cache in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Booster2013::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.cache_store = :memory_store
   
   config.action_mailer.default_url_options = {
