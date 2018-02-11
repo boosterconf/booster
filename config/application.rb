fdbea4087f1ab184bce9b0b2ec3a38d3f35211ea
Karianne Berg <karianne.berg@gmail.com>
2018-02-11 14:19:14 +0100

    remove deprecated config setting

--- config/application.rb
+++ config/application.rb
@@ -57,9 +57,6 @@ module Booster2013
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
-
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
