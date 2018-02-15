91ac7a06dc216b9cf901e4b88cea37304abd95fd
Karianne Berg <karianne.berg@gmail.com>
2018-02-15 22:03:49 +0100

    Fix bug that caused all app config to be test env

--- config/initializers/load_payment_config.rb
+++ config/initializers/load_payment_config.rb
@@ -4,4 +4,4 @@ require 'yaml'
 # Lager et objekt (strengt tatt en struct) av konfigurasjonen - gjør at du kan gjøre fx 'AppConfig.max_users_limit'
 # Dette gjør det mulig å mocke kall til konfigurasjonen.
 config = OpenStruct.new(YAML.load_file("#{Rails.root}/config/app_config.yml"))
-::AppConfig = OpenStruct.new(config.public_send(:test))
+::AppConfig = OpenStruct.new(config.public_send(Rails.env))
