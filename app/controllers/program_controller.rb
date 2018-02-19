310afe08fc676e65926a5cdca324b5b88445af1b
Anders Rørvik <anders.rorvik@gmail.com>
2018-02-19 20:18:52 +0100

    is_admin?

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,14 +1,14 @@
 class ProgramController < ApplicationController
 
   def index
-    unless AppConfig.program_released || current_user.is_admin
+    unless AppConfig.program_released || current_user.is_admin?
       return redirect_to root_path
     end
 
     all_talks = Talk.all
     @talks = {}
-    all_talks.each { |talk| @talks[talk.id] = talk }
+    all_talks.each {|talk| @talks[talk.id] = talk}
 
-    @periods = Period.all.sort_by { |period| [period.day, period.start_time] }.reverse
+    @periods = Period.all.sort_by {|period| [period.day, period.start_time]}.reverse
   end
 end
