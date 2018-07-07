0aa53511d63415d8d66de7d502d0f88a848fc88f
Anders Rørvik <anders.rorvik@gmail.com>
2018-07-08 00:15:43 +0200

    actually apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_213745) do
+ActiveRecord::Schema.define(version: 2018_07_07_215428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
