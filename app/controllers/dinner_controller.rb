e173d8dc21e14cfb2d3769189678f267805b3ea6
Anders Rørvik <anders.rorvik@gmail.com>
2018-07-09 21:33:41 +0200

    the dinner controller now uses tickets instead of registrations like it should :-D

--- app/controllers/dinner_controller.rb
+++ app/controllers/dinner_controller.rb
@@ -5,15 +5,9 @@ class DinnerController < ApplicationController
   before_action :require_admin
 
   def index
-    @registrations = Registration.includes(:ticket_type).all
-    respond_with @registrations
-  end
-
-  # todo fix attending listing
-  def attend_conference_dinner
-  end
-
-  # todo fix attend speakers dinner listing
-  def attend_speakers_dinner
+    @tickets = Ticket.includes(:ticket_type).all
+    @totalAttendingConferenceDinner = @tickets.count {|t| t.attend_dinner}
+    @totalAttendingSpeakersDinner = @tickets.count {|t| t.attend_speakers_dinner}
+    respond_with @tickets
   end
 end
