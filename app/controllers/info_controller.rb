841b1c43e243b43824eebdf29d9dbfdee1aa2e62
Anders Rørvik <anders.rorvik@gmail.com>
2018-07-09 20:36:48 +0200

    remove obsolete comment

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,15 +1,4 @@
-# Controller for displaying static html pages
-# See http://railscasts.com/episodes/117-semi-static-pages for details
 class InfoController < ApplicationController
-
-  # Page cache for all views.
-  # See http://www.railsenvy.com/2007/2/28/rails-caching-tutorial for details
-  #caches_page :index, :arrangoerene, :lyntaler, :openspace
-
-  #  def applyCacheControl
-  #    response.headers['Cache-Control'] = 'public, max-age=3600'
-  #  end
-
   def speakers
     @speakers = User.all_confirmed_speakers
   end
