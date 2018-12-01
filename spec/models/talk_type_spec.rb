7e94a2e4bdd4be2f1431b466134fab84cd99e5dc
Anders Rørvik <anders.rorvik@gmail.com>
2018-12-01 23:32:53 +0100

    OCD TLC

--- spec/models/talk_type_spec.rb
+++ spec/models/talk_type_spec.rb
@@ -1,39 +1,32 @@
 require 'rails_helper'
 
 describe TalkType, type: :model do
+  def create_sut(name)
+    talk_type = TalkType.new
+    talk_type.name = name
+    talk_type
+  end
   describe "is_keynote?" do
     it "is false when name is Lightning talk" do
-      talk_type = TalkType.new
-      talk_type.name = 'Lightning talk'
-      expect(talk_type.is_keynote?).to be false
+      expect(create_sut('Lightning talk').is_keynote?).to be false
     end
     it "is false when name is Short talk" do
-      talk_type = TalkType.new
-      talk_type.name = 'Short talk'
-      expect(talk_type.is_keynote?).to be false
+      expect(create_sut('Short talk').is_keynote?).to be false
     end
     it "is true when name is Keynote" do
-      talk_type = TalkType.new
-      talk_type.name = 'Keynote'
-      expect(talk_type.is_keynote?).to be true
+      expect(create_sut('Keynote').is_keynote?).to be true
     end
 
   end
   describe "is_lightning_talk?" do
     it "is false when Short talk" do
-      talk_type = TalkType.new
-      talk_type.name = 'Short talk'
-      expect(talk_type.is_lightning_talk?).to be false
+      expect(create_sut('Short talk').is_lightning_talk?).to be false
     end
     it "is false when Keynote" do
-      talk_type = TalkType.new
-      talk_type.name = 'Keynote'
-      expect(talk_type.is_lightning_talk?).to be false
+      expect(create_sut('Keynote').is_lightning_talk?).to be false
     end
     it "is true when Lightning talk" do
-      talk_type = TalkType.new
-      talk_type.name = 'Lightning talk'
-      expect(talk_type.is_lightning_talk?).to be true
+      expect(create_sut('Lightning talk').is_lightning_talk?).to be true
     end
   end
 end
