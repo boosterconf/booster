21e01c2e691f23ac5fdb2ae3fac109430cca168e
Anders Rørvik <anders.rorvik@gmail.com>
2018-12-01 23:25:53 +0100

    test is_lightning_talk? on talktype model

--- spec/models/talk_type_spec.rb
+++ spec/models/talk_type_spec.rb
@@ -17,5 +17,23 @@ describe TalkType, type: :model do
       talk_type.name = 'Keynote'
       expect(talk_type.is_keynote?).to be true
     end
+
+  end
+  describe "is_lightning_talk?" do
+    it "is false when Short talk" do
+      talk_type = TalkType.new
+      talk_type.name = 'Short talk'
+      expect(talk_type.is_lightning_talk?).to be false
+    end
+    it "is false when Keynote" do
+      talk_type = TalkType.new
+      talk_type.name = 'Keynote'
+      expect(talk_type.is_lightning_talk?).to be false
+    end
+    it "is true when Lightning talk" do
+      talk_type = TalkType.new
+      talk_type.name = 'Lightning talk'
+      expect(talk_type.is_lightning_talk?).to be true
+    end
   end
 end
