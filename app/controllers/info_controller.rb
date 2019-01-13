5b18ba63d303beeda324222ddf94f1c8546fc29f
Anders RÃrvik <anders.rorvik@gmail.com>
2019-01-13 20:54:54 +0100

    show remaining number of early bird tickets

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -5,6 +5,9 @@ class InfoController < ApplicationController
 
   def index
     @featured_speakers = User.featured_speakers.shuffle[0...6]
+    if(TicketType.early_bird_is_active?)
+      @remaining_early_bird_ticket_count = AppConfig.early_bird_ticket_limit - Ticket.count_by_ticket_type(TicketType.early_bird)
+    end
   end
 
   def about
