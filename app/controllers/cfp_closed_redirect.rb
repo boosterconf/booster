f0438cda6c3cf096bbe7d379981303bc94b9f214
Anders Rørvik <anders.rorvik@spv.no>
2019-01-14 19:00:29 +0100

    always allow registering talks if admin

--- app/controllers/cfp_closed_redirect.rb
+++ app/controllers/cfp_closed_redirect.rb
@@ -9,12 +9,18 @@ module CfpClosedRedirect
   end
 
   def redirect_when_cfp_closed_for_lightning_talks
+    if current_user && current_user.is_admin
+      return
+    end
     if is_lighting_talk_open?
       redirect_to :root
     end
   end
 
   def redirect_when_cfp_closed_for_workshops
+    if current_user && current_user.is_admin
+      return
+    end
     if is_workshop_open?
       redirect_to :root
     end
