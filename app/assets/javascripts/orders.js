c9962613986babc7ffa2075bbdf6347959873f38
Håkon H. Lerring <hakon@lerring.no>
2019-03-03 21:48:50 +0100

    Remove debug in orders.js

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -4,11 +4,9 @@ $(document).ready(function() {
 			if($(this).prop("value") == "true") {
 				$('#new-sale-fieldset').show();
 				$('#existing-sale-fieldset').hide();
-				console.log("checked");
 			} else {
 				$('#new-sale-fieldset').hide();
 				$('#existing-sale-fieldset').show();
-				console.log("not checked");
 			}
 		}
 	}).change();
