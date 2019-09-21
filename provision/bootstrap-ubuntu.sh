c14126c93f2e758603de61c76734b7953670bcc4
Anders Rørvik <anders.rorvik@websystemer.as>
2019-09-21 14:30:41 +0200

    upgrade vagrant setup

--- provision/bootstrap-ubuntu.sh
+++ provision/bootstrap-ubuntu.sh
@@ -6,3 +6,5 @@ sudo update-locale LC_ALL=en_US.UTF-8
 sudo apt-get update
 sudo apt-get install -y build-essential git curl libxslt1-dev libxml2-dev libssl-dev git
 sudo apt-get install -y imagemagick
+sudo apt-get install gnupg2 -y
+
--- provision/install-rvm.sh
+++ provision/install-rvm.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
+echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust # piotr.kuczynski@gmail.com
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s $1
