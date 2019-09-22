21e7dd903b8c05158394adbc79f6eb0c810aaa94
Håkon H. Lerring <hakon@lerring.no>
2019-09-22 08:30:22 +0200

    Don't let users register outside of the ticket / cfp flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :encryptable, :database_authenticatable, :registerable,
+  devise :encryptable, :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   acts_as_paranoid
