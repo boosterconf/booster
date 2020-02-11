75b9a01edfd79385252fa6bbc5d4f6ca9e9ebbb0
Håkon H. Lerring <hakon@lerring.no>
2020-02-11 18:46:02 +0100

    fix sidekiq admin requirement

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   require 'sidekiq/web'
 
-  authenticate :admin do
+  authenticate :user, lambda { |u| u.is_admin } do
     mount Sidekiq::Web => '/sidekiq'
   end
-
   devise_for :users
   get '/.well-known/acme-challenge/T0USF7R5JLNo9xygAzV_mHXxetcd2HLWD3Gjz3uNSiU' => 'pages#letsencrypt'
 
