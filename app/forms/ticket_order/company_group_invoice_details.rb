4f87ae314a1394fddec0f32cfe49b1cac9669d8a
Håkon H. Lerring <hakon@lerring.no>
2020-02-13 17:11:02 +0100

    Fix organizationIdentifier validation

--- app/forms/ticket_order/company_group_invoice_details.rb
+++ app/forms/ticket_order/company_group_invoice_details.rb
@@ -11,7 +11,13 @@ class CompanyGroupInvoiceDetails
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :organizationIdentifier, presence: true, length: { is: 9 }
+  validates :organizationIdentifier, presence: true
+
+  validate :organizationIdentifier do
+    if(organizationIdentifier.gsub(/[^\d]/, "").length != 9)
+      errors.add :organizationIdentifier, 'should be exactly 9 digits'
+    end
+  end
 
 end
 end
